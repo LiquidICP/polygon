8f9bc2be710bc333957762d6d463d3276b0e6576
IntegralTeam <repo@sfxdx.ru>
2022-01-18 10:29:50 +0300

    fix bridge

--- contracts/bridge/Bridge.sol
+++ contracts/bridge/Bridge.sol
@@ -5,31 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../interfaces/IBridge.sol";
 import "../interfaces/IWrapperBridgedStandardERC20.sol";
-// добавить изменяемую комиссию
-// добавить соотношение кошельков
-// проработать взятие комиссии
+
 contract Bridge is AccessControl, IBridge {
     using SafeERC20 for IERC20;
 
     address public wallerForBurning;
 
+    uint public feeRate;
+    uint public constant MAX_BP = 1000;
+
     IWrappedInternetComputerToken public iWrappedInternetComputerToken;
 
     uint public commission;
-//    bytes32 public constant ICP_ADDRESS = "";
-//
-//    mapping (byte32 => address) relatedTokens;
 
     modifier onlyAdmin() {
         require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "onlyAdmin");
         _;
     }
 
-    constructor (address _wrappedICP, address _wallerForBurning, uint _commission) {
+    constructor (address _wrappedICP, address _wallerForBurning, uint _feeRate) {
         require(wallerForBurning != address(0), "The wallet address must not be 0 or empty");
         require(_wrapperICP != address(0), "The token address must not be 0 or empty");
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        commission = _commission;
+        feeRate = _feeRate;
         wallerForBurning = _wallerForBurning;
         iWrappedInternetComputerToken = IWrappedInternetComputerToken(_wrappedICP);
     }
@@ -38,24 +36,25 @@ contract Bridge is AccessControl, IBridge {
         grantRole(DEFAULT_ADMIN_ROLE, _newAdmin);
     }
 
-    function setCommission(uint _commission) external onlyAdmin {
-        commission = _commission;
-    }
-    //нужно ли выделять сбор комиссии в отдельную функцию?
-    function forwardTokensFromPolygonToDfinity(uint _amount, bytes32 _address) external {
-        uint commissionAmount = calcCommissionAmount(_amount, commission);
-        address sender = _msgSender();
-        iWrappedInternetComputerToken.safeTransferFrom(sender, address(this), commissionAmount);
-        iWrappedInternetComputerToken.burn(sender, _wallerForBurning, _amount - commissionAmount);
-        emit ForwardTokensFromPolygonToDfinity(_wallerForBurning, _amount);
+    function setFeeRate(uint _feeRate) external onlyAdmin {
+        feeRate = _feeRate;
     }
 
-    function calcCommissionAmount(uint _amount, uint _commission) internal pure returns(uint){
-        return _amount % _commission;
+    function forwardTokensFromPolygonToDfinity(uint _amount, bytes32 _address) external {
+        uint feeAmount = calcFeeAmount(_amount);
+        address sender = _msgSender();
+        iWrappedInternetComputerToken.safeTransferFrom(sender, address(this), feeAmount);
+        iWrappedInternetComputerToken.burn(sender, _wallerForBurning, _amount - feeAmount);
+        emit ForwardTokensFromPolygonToDfinity(_wallerForBurning, _amount - feeAmount);
+    }
+
+    function calcFeeAmount(uint _amount) private pure returns(uint){
+        return _amount * feeRate / MAX_BP;
     }
 
     function mintTokens(address _to, uint _amount) external {
         iWrappedInternetComputerToken.mint(_to, _amount);
         emit ForwardTokensFromPolygonToDfinity(_to, _amount);
     }
+
 }
