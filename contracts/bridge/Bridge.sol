13848b2bde3f3e40501a999b469fa813845344cb
IntegralTeam <repo@sfxdx.ru>
2022-01-20 17:22:17 +0300

    review token and bridge. Fix commission

--- contracts/bridge/Bridge.sol
+++ contracts/bridge/Bridge.sol
@@ -1,24 +1,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../interfaces/IBridge.sol";
 import "../interfaces/IWrapperBridgedStandardERC20.sol";
 
 contract Bridge is AccessControl, IBridge {
     using SafeERC20 for IERC20;
+    using Clones for address;
 
-    address public wallerForBurning;
+    address public wallerForFee;
 
     uint public feeRate;
     uint public constant MAX_BP = 1000;
 
-    IWrappedInternetComputerToken public iWrappedInternetComputerToken;
+    IWrapperBridgedStandardERC20 public iWrapperBridgedStandardERC20;
 
-    mapping(address => bool) public allowedTokens;
-//
-//    mapping (byte32 => address) relatedTokens;
+    bytes32 public constant BOT_MESSANGER_ROLE = keccak256("BOT_MESSANGER_ROLE");
+    //
+    //    mapping (byte32 => address) relatedTokens;
 
     modifier onlyAdmin() {
         require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "onlyAdmin");
@@ -30,16 +33,11 @@ contract Bridge is AccessControl, IBridge {
         _;
     }
 
-    modifier tokenIsAllowed(address _token) {
-        require(allowedTokens[_token], "invalidToken");
-        _;
-    }
-
     constructor (
         address _wrappedICP,
-        address _wallerForBurning,
-        uint _feeRate, address _botMessanger,
-        address _allowedToken,
+        address _wallerForFee,
+        uint _feeRate,
+        address _botMessanger,
         string memory _name,
         string memory _symbol
     ) {
@@ -48,17 +46,16 @@ contract Bridge is AccessControl, IBridge {
         feeRate = _feeRate;
 
         if (_wrappedICP != address(0)) {
-            iWrappedInternetComputerToken = IWrapperBridgedStandardERC20(_wrappedICP);
+            iWrapperBridgedStandardERC20 = IWrapperBridgedStandardERC20(_wrappedICP);
         }
-        if (_wallerForBurning != address(0)) {
-            wallerForBurning = _wallerForBurning;
+        if (_wallerForFee != address(0)) {
+            wallerForFee = _wallerForFee;
         }
-        _cloneAndInitializeTokenAtEndForTokenAtStart(_allowedToken, _name, _symbol);
+        _cloneAndInitializeTokenAtEndForTokenAtStart(_name, _symbol);
     }
 
-    function evacuateTokens(address _token, uint256 _amount, address _to) external onlyAdmin {
-        require(!allowedTokens[_token], "cannotEvacuateAllowedToken");
-        IERC20(_token).safeTransfer(_to, _amount);
+    function evacuateTokens(uint256 _amount, address _to) external onlyAdmin {
+        IERC20(address(iWrapperBridgedStandardERC20)).safeTransfer(_to, _amount);
     }
 
     function setAdmin(address _newAdmin) external onlyAdmin {
@@ -70,66 +67,55 @@ contract Bridge is AccessControl, IBridge {
     }
 
     function requestBridgingToStart(
-        address _tokenAtStart,
-        address _tokenAtEnd,
-        address _to,
+        address _token,
         uint _amount
-    ) external override onlyAtEnd tokenIsAllowed(_tokenAtEnd) {
+    ) external override {
         uint feeAmount = calcFeeAmount(_amount);
         address sender = _msgSender();
-        iWrappedInternetComputerToken(_tokenAtEnd).safeTransferFrom(sender, address(this), feeAmount);
-        iWrappedInternetComputerToken.burn(sender, _wallerForBurning, _amount - feeAmount);
-        emit RequestBridgingToStart(_tokenAtStart, _tokenAtEnd, sender, _to, _amount - feeAmount);
+        IWrapperBridgedStandardERC20(_token).transferFrom(sender, wallerForFee, feeAmount);
+        IWrapperBridgedStandardERC20(_token).burn(sender, _amount - feeAmount);
+        emit RequestBridgingToStart(_token, sender, _amount - feeAmount);
     }
 
-    function calcFeeAmount(uint _amount) private pure returns(uint){
+    function calcFeeAmount(uint _amount) private view returns(uint){
         return _amount * feeRate / MAX_BP;
     }
 
     function performBridgingToEnd(
-        address _tokenAtStart,
         address _to,
         uint256 _amount,
         string memory _name,
         string memory _symbol
     )
-        external
-        override
-        onlyAtEnd
-        onlyMessangerBot
+    external
+    override
+    onlyMessangerBot
     {
-        address tokenAtEnd = getEndTokenByStartToken(_tokenAtStart);
-        if (tokenAtEnd == address(0)) {
-            tokenAtEnd = _cloneAndInitializeTokenAtEndForTokenAtStart(
-                _tokenAtStart,
+        address token = address(iWrapperBridgedStandardERC20);
+        if (token == address(0)) {
+            token = _cloneAndInitializeTokenAtEndForTokenAtStart(
                 _name,
                 _symbol
             );
         }
-        iWrappedInternetComputerToken(tokenAtEnd).mint(_to, _amount);
-        emit BridgingToEndPerformed(_tokenAtStart, tokenAtEnd, _to, _amount);
+        iWrapperBridgedStandardERC20.mint(_to, _amount);
+        emit BridgingToEndPerformed(token, _to, _amount);
     }
 
     function _cloneAndInitializeTokenAtEndForTokenAtStart(
-        address _tokenAtStart,
         string memory _name,
         string memory _symbol
     )
     internal
-    returns(address tokenAtEnd)
+    returns(address token)
     {
-        tokenAtEnd = address(bridgedStandartERC20).clone();
-        tokenPairs.push(Pair({
-        tokenAtStart: _tokenAtStart,
-        tokenAtEnd: tokenAtEnd
-        }));
-        allowedTokens[tokenAtEnd] = true;
-        IWrapperBridgedStandardERC20(tokenAtEnd).configure(
+        address _token = Clones.clone(address(iWrapperBridgedStandardERC20));
+        IWrapperBridgedStandardERC20(_token).configure(
             address(this),
-            _tokenAtStart,
+            _token,
             _name,
             _symbol
         );
+        return _token;
     }
-
 }
--- contracts/interfaces/IBridge.sol
+++ contracts/interfaces/IBridge.sol
@@ -3,29 +3,23 @@ pragma solidity ^0.8.0;
 interface IBridge {
 
     event RequestBridgingToStart(
-        address indexed _tokenAtStart,
-        address indexed _tokenAtEnd,
+        address indexed _token,
         address indexed _from,
-        address _to,
         uint256 _amount
     );
 
     event BridgingToEndPerformed(
-        address indexed _tokenAtStart,
-        address indexed _tokenAtEnd,
+        address indexed _token,
         address indexed _to,
         uint256 _amount
     );
 
     function requestBridgingToStart(
-        address _tokenAtStart,
-        address _tokenAtEnd,
-        address _to,
+        address _token,
         uint256 _amount
     ) external;
 
     function performBridgingToEnd(
-        address _tokenAtStart,
         address _to,
         uint256 _amount,
         string memory _name,
